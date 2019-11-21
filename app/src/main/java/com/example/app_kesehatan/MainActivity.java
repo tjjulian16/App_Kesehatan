2175796296363cb200e9b9843e4b40234a6e116d
Julian <timothy_julian@ymail.com>
2019-11-21 16:47:59 +0700

    Login Register Harusnya Udah bisa

--- app/src/main/java/com/example/app_kesehatan/MainActivity.java
+++ app/src/main/java/com/example/app_kesehatan/MainActivity.java
@@ -1,16 +1,81 @@
 package com.example.app_kesehatan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText emailId, password;
+    Button btnSignUp;
+    TextView textLogin;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        BuatData BuatData = new BuatData(this);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.inputEmail);
+        password = findViewById(R.id.inputPass);
+        btnSignUp = findViewById(R.id.btnRegistrasi);
+        textLogin = findViewById(R.id.loginDisini);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String pwd = password.getText().toString();
+                if(email.isEmpty()){
+                    emailId.setError("Harap masukkan email");
+                    emailId.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    password.setError("Harap masukkan Password");
+                    password.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Email dan Password harus diisisi",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(MainActivity.this,"Pendaftaran Gagal",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(MainActivity.this,"Pendaftaran Berhasil, Silahkan Login",Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"Terjadi Kesalahan!",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        textLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
