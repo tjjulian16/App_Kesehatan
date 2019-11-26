b94330717f8bf61dc7090bd449a69d8fc3839ca8
Julian <timothy_julian@ymail.com>
2019-11-26 12:25:42 +0700

    Update dataKelurahan.java

--- app/src/main/java/com/example/app_kesehatan/dataKelurahan.java
+++ app/src/main/java/com/example/app_kesehatan/dataKelurahan.java
@@ -8,6 +8,7 @@ public class dataKelurahan {
             "Jatimulyo",
             "Ketawanggede",
             "Lowokwaru",
+            "Merjosari",
             "Mojolangu",
             "Sumbersari",
             "Tasikmadu",
