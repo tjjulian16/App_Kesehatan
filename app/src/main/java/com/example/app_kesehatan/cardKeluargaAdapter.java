0ef92ad8dda7fc7d5e7c61d9897cbd6a6c60c502
alxjesse <alxjesse@github.com>
2019-11-26 21:19:02 +0700

    tambahan dikit

---
+++ app/src/main/java/com/example/app_kesehatan/cardKeluargaAdapter.java
@@ -0,0 +1,16 @@
+package com.example.app_kesehatan;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class cardKeluargaAdapter extends RecyclerView.Adapter<cardKeluargaAdapter> {
+
+    private ArrayList<> listKeluarga;
+    private Context context;
+
+    public RecyclerViewAdapter(ArrayList<kesehatan>) listKesehatan
+
+}
